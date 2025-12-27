9dd86300cf8d0bd23de25e6ed436193e9b153f7f
Praveen De Silva <desilvabkp.23@uom.lk>
2025-12-27 22:04:24 +0530

    Home page completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="relative w-full py-6 px-6">
       <div className="absolute top-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-white to-transparent opacity-30"></div>
       <div className="container mx-auto text-center">
-        <p className="text-sm font-light text-gray-300">
+        <p className="text-sm font-light text-gray-300 opacity-50">
           Praveen | December 2025
         </p>
       </div>
